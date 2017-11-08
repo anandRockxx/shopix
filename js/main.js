592e9b35f74a23c06e2e71e2e30f25967a1a5fa1
jekyllx <anand.pal2050@gmail.com>
2017-11-08 11:14:00 +0530

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,100 +1,100 @@
 // Slider1 Script
 $("#slider1").owlCarousel({
-  loop: true,
-  margin: 70,
-  autoplay: true,
-  autoplayTimeout: 3000,
-  autoplayHoverPause: false,
-  nav: false,
-  dots: false,
-  responsive: {
-    0: {
-      items: 1
-    },
-    719:{
-      items:1
-    },
-    720: {
-      items: 2
-    },
-    1025: {
-      items: 3
+    loop: true,
+    margin: 70,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: false,
+    nav: false,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        719: {
+            items: 1
+        },
+        720: {
+            items: 2
+        },
+        1025: {
+            items: 3
+        }
     }
-  }
 });
 
 
 // slider2 Script
 $('#slider2').owlCarousel({
-  lazyLoad: true,
-  loop: true,
-  autoplayHoverPause: false,
-  autoplay: true,
-  autoplayTimeout: 3000,
-  margin: 50,
-  center: true,
-  autoplayHoverPause: true,
-  responsive: {
-    0: {
-      items: 1
-    },
-    720: {
-      items: 2
-    },
-    1140: {
-      items: 2
+    lazyLoad: true,
+    loop: true,
+    autoplayHoverPause: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    margin: 50,
+    center: true,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        720: {
+            items: 2
+        },
+        1140: {
+            items: 2
+        }
     }
-  }
 });
 
 // Brand Script
 $('#brand').owlCarousel({
-  loop: true,
-  autoplay: true,
-  autoplayTimeout: 2000,
-  margin: 100,
-  responsive: {
-    0: {
-      items: 1
-    },
-    600: {
-      items: 3
-    },
-    1000: {
-      items: 5
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    margin: 100,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
     }
-  }
 });
 
 // popup script
 $('.zoom-gallery').magnificPopup({
-  delegate: 'a',
-  type: 'image',
-  closeOnContentClick: false,
-  closeBtnInside: false,
-  mainClass: 'mfp-with-zoom mfp-img-mobile',
-  image: {
-    verticalFit: true,
-    titleSrc: function(item) {
-      return item.el.attr('title');
+    delegate: 'a',
+    type: 'image',
+    closeOnContentClick: false,
+    closeBtnInside: false,
+    mainClass: 'mfp-with-zoom mfp-img-mobile',
+    image: {
+        verticalFit: true,
+        titleSrc: function(item) {
+            return item.el.attr('title');
+        }
+    },
+    gallery: {
+        enabled: true
+    },
+    zoom: {
+        enabled: true,
+        duration: 300,
+        opener: function(element) {
+            return element.find('img');
+        }
     }
-  },
-  gallery: {
-    enabled: true
-  },
-  zoom: {
-    enabled: true,
-    duration: 300,
-    opener: function(element) {
-      return element.find('img');
-    }
-  }
 });
 
 //smooth scrolling
 $(document).on('click', 'a', function(event) {
-  event.preventDefault();
-  $('html, body').animate({
-    scrollTop: $($.attr(this, 'href')).offset().top
-  }, 800);
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 800);
 });
